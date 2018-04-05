44b8d4eb56f5346172b3c9f0f74084c0f8b50c07
Sam Windham <sam.windham@arrow.com>
2018-04-05 17:32:37 -0600

    marked set up

--- src/App/Main/Editor/Line.jsx
+++ src/App/Main/Editor/Line.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import style from './Line.css';
+import marked from 'marked';
 
 class Line extends Component {
   render() {
-    const line = this.props.text ? this.props.text: <br/>;
+    const line = this.props.text ? marked(this.props.text): '<br/>';
 
     return (
-      <div className={style.line}>
-        {line}
-      </div>
+      <div className={style.line} dangerouslySetInnerHTML={{__html: line}}></div>
     )
   }
 }
